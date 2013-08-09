d7d24983439bd9a4d537917ac904e7eec07906cc
Andrew Burgess <andrew.burgess@embecosm.com>
2013-08-09 17:11:08 +0100

    gcc/arc: Add support for nps400 cmem xld/xst instructions
    This commit adds support for NPS400 cmem memory sections.  Data to be
    placed into cmem memory is placed into a section ".cmem",
    ".cmem_shared", or ".cmem_private".
    
    There are restrictions on how instructions can be used to operate on
    data held in cmem memory, this is reflected by the introduction of new
    operand constraints (Uex/Ucm), and modifications to some instructions to
    make use of these constraints.
    
    gcc/ChangeLog:
    
    	* config/arc/arc.h (SYMBOL_FLAG_CMEM): Define.
    	(TARGET_NPS400_CMEM_DEFAULT): Provide default definition.
    	* config/arc/arc.c (arc_address_cost): Return 0 for cmem_address.
    	(arc_encode_section_info): Set SYMBOL_FLAG_CMEM where indicated.
    	* config/arc/arc.opt (mcmem): New option.
    	* config/arc/arc.md (*extendqihi2_i): Add r/Uex alternative,
    	supply length for r/m alternative.
    	(*extendqisi2_ac): Likewise.
    	(*extendhisi2_i): Add r/Uex alternative, supply length for r/m and
    	r/Uex alternative.
    	(movqi_insn): Add r/Ucm and Ucm/?Rac alternatives.
    	(movhi_insn): Likewise.
    	(movsi_insn): Add r/Ucm,Ucm/w alternatives.
    	(*zero_extendqihi2_i): Add r/Ucm alternative.
    	(*zero_extendqisi2_ac): Likewise.
    	(*zero_extendhisi2_i): Likewise.
    	* config/arc/constraints.md (Uex): New memory constraint.
    	(Ucm): New define_constraint.
    	* config/arc/predicates.md (long_immediate_loadstore_operand):
    	Return 0 for MEM with cmem_address address.
    	(cmem_address_0): New predicates.
    	(cmem_address_1): Likewise.
    	(cmem_address_2): Likewise.
    	(cmem_address): Likewise.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/arc/cmem-1.c: New file.
    	* gcc.target/arc/cmem-2.c: New file.
    	* gcc.target/arc/cmem-3.c: New file.
    	* gcc.target/arc/cmem-4.c: New file.
    	* gcc.target/arc/cmem-5.c: New file.
    	* gcc.target/arc/cmem-6.c: New file.
    	* gcc.target/arc/cmem-7.c: New file.
    	* gcc.target/arc/cmem-ld.inc: New file.
    	* gcc.target/arc/cmem-st.inc: New file.

---
+++ gcc/testsuite/gcc.target/arc/cmem-4.c
@@ -0,0 +1,10 @@
+/* { dg-do compile { target arc*-mellanox-* } } */
+/* { dg-options "-mcmem" } */
+
+#define CMEM_SECTION_ATTR __attribute__ ((section (".cmem_private")));
+
+#include "cmem-ld.inc"
+
+/* { dg-final { scan-assembler "xld " } } */
+/* { dg-final { scan-assembler "xldw " } } */
+/* { dg-final { scan-assembler "xldb " } } */
---
+++ gcc/testsuite/gcc.target/arc/cmem-5.c
@@ -0,0 +1,10 @@
+/* { dg-do compile { target arc*-mellanox-* } } */
+/* { dg-options "-mcmem" } */
+
+#define CMEM_SECTION_ATTR __attribute__ ((section (".cmem_shared")));
+
+#include "cmem-st.inc"
+
+/* { dg-final { scan-assembler "xst " } } */
+/* { dg-final { scan-assembler "xstw " } } */
+/* { dg-final { scan-assembler "xstb " } } */
